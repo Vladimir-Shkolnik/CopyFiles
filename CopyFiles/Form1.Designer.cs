062fa70138d473d21653b598d2fbe1549f39bdde
Vladimir-Shkolnik <scholnick2012@gmail.com>
2017-02-15 01:14:56 +0200

    Programm for copy files

---
+++ CopyFiles/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace CopyFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.open_File = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(393, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Copy from file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 50);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 108);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Copy Files from :";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(393, 135);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Copy all";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // open_File
+            // 
+            this.open_File.Location = new System.Drawing.Point(393, 34);
+            this.open_File.Name = "open_File";
+            this.open_File.Size = new System.Drawing.Size(75, 23);
+            this.open_File.TabIndex = 4;
+            this.open_File.Text = "TXT File";
+            this.open_File.UseVisualStyleBackColor = true;
+            this.open_File.Click += new System.EventHandler(this.open_File_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(172, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Файл :";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 16);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = ".jpg file";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 39);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(55, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.Text = ".txt file";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 60);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(59, 17);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.Text = ".pdf file";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 19);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(108, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "look in subfolders";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Location = new System.Drawing.Point(173, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(68, 106);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(6, 42);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(69, 17);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "overwrite";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 194);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(453, 23);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(6, 65);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(111, 17);
+            this.checkBox3.TabIndex = 11;
+            this.checkBox3.Text = "delete source files";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox3);
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Controls.Add(this.checkBox2);
+            this.groupBox2.Location = new System.Drawing.Point(247, 52);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(119, 106);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 246);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.open_File);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CopyFiles";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button open_File;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ CopyFiles/Form1.cs
@@ -0,0 +1,389 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace CopyFiles
+{
+    public partial class Form1 : Form
+    {
+        List<string> _items = new List<string>();
+        string selectfile = "";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            //https://msdn.microsoft.com/ru-ru/library/system.windows.forms.progressbar(v=vs.110).aspx
+            //http://csharp.net-informations.com/gui/cs-radiobutton.htm
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //http://professorweb.ru/my/csharp/thread_and_files/level3/3_6.php
+            //https://msdn.microsoft.com/ru-ru/library/system.io.file.delete(v=vs.110).aspx
+            //http://stackoverflow.com/questions/771564/how-to-create-a-sub-directory-inside-a-directory
+
+        }
+
+        string sourceDir = AppDomain.CurrentDomain.BaseDirectory;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (selectfile != "" && selectfile != null)
+            {
+                string[] picList;
+                string backupDir = sourceDir + "output";
+                string errorFile = "Error.txt";
+                string errorFilePatch = Path.Combine(backupDir, errorFile);
+
+                if (!Directory.Exists(backupDir))
+                {
+                    Directory.CreateDirectory(backupDir);
+                }
+
+                try
+                {
+                    SearchOption searchOption;
+                    bool overwrite;
+                    if (checkBox1.Checked == true)
+                    {
+                        searchOption = SearchOption.AllDirectories;
+                    }
+                    else
+                    {
+                        searchOption = SearchOption.TopDirectoryOnly;
+                    }
+                    if (checkBox2.Checked == true)
+                    {
+                        overwrite = true;
+                    }
+                    else
+                    {
+                        overwrite = false;
+                    }
+
+                    if (radioButton1.Checked == true)
+                    {
+                        picList = Directory.GetFiles(sourceDir, "*.jpg", searchOption);
+                    }
+                    else if (radioButton2.Checked == true)
+                    {
+                        picList = Directory.GetFiles(sourceDir, "*.txt", searchOption);
+                    }
+                    else
+                    {
+                        picList = Directory.GetFiles(sourceDir, "*.pdf", searchOption);
+                    }
+                    List<string> notfound = new List<string>();
+                    int countError = 0;
+                    int countSuccess = 0;
+                    string val = "";
+                    progressBar1.Visible = true;
+                    progressBar1.Minimum = 1;
+                    progressBar1.Maximum = _items.Count;
+                    progressBar1.Value = 1;
+                    progressBar1.Step = 1;
+                    var watch = Stopwatch.StartNew();
+                    foreach (var item in _items)
+                    {
+                        val = progressBar1.Value.ToString();
+                        progressBar1.PerformStep();
+                        val = progressBar1.Value.ToString();
+
+                        //CopyWithProgress(countSuccess);
+                        //CopyWithProgress(picList);
+                        //CopyWithProgress(picList.Length);
+                        string name = sourceDir + item;
+                        if (picList.Contains(name))
+                        {
+                            try
+                            {
+                                File.Copy(Path.Combine(sourceDir, item), Path.Combine(backupDir, item), overwrite);
+                                countSuccess += 1;
+                            }
+                            catch (Exception)
+                            {
+                                continue;
+                            }
+                        }
+                        else
+                        {
+                            countError += 1;
+                            notfound.Add(item);                            
+                            string data = item + Environment.NewLine;
+                            File.AppendAllText(errorFile, data);
+                        }
+                    }
+                    string[] copiedList = Directory.GetFiles(backupDir);
+                    if (checkBox3.Checked == true)
+                    {
+                        foreach (string f in copiedList)
+                        {
+                            string name = sourceDir + Path.GetFileName(f); ;
+                            File.Delete(name);
+                        }
+                    }
+                    else
+                    {
+
+                    }
+                    watch.Stop();
+                    var elapsedMs = watch.ElapsedMilliseconds;
+
+                    TimeSpan ts = watch.Elapsed;
+                    string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+
+                    MessageBox.Show("Elapsed time" + elapsedTime + Environment.NewLine +
+                        "Copied successfully: " + countSuccess + Environment.NewLine +
+                        "Not found - " + countError + " files: " + Environment.NewLine +
+                        string.Join(Environment.NewLine, notfound.ToArray()), "Error");
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Pleasure select file");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (selectfile != "" && selectfile != null)
+            {
+                string[] picList;
+                string backupDir = sourceDir + "output";
+                string errorFile = "Error.txt";
+                string errorFilePatch = Path.Combine(backupDir, errorFile);
+
+                if (!Directory.Exists(backupDir))
+                {
+                    Directory.CreateDirectory(backupDir);
+                }
+
+                try
+                {
+                    SearchOption searchOption;
+                    bool overwrite;
+                    if (checkBox1.Checked == true)
+                    {
+                        searchOption = SearchOption.AllDirectories;
+                    }
+                    else
+                    {
+                        searchOption = SearchOption.TopDirectoryOnly;
+                    }
+                    if (checkBox2.Checked == true)
+                    {
+                        overwrite = true;
+                    }
+                    else
+                    {
+                        overwrite = false;
+                    }
+
+                    picList = Directory.GetFiles(sourceDir);
+
+                       
+                    List<string> notfound = new List<string>();
+                    int countError = 0;
+                    int countSuccess = 0;
+                    string val = "";
+                    progressBar1.Visible = true;
+                    progressBar1.Minimum = 1;
+                    progressBar1.Maximum = _items.Count;
+                    progressBar1.Value = 1;
+                    progressBar1.Step = 1;
+                    var watch = Stopwatch.StartNew();
+                    foreach (var item in _items)
+                    {
+                        val = progressBar1.Value.ToString();
+                        progressBar1.PerformStep();
+                        val = progressBar1.Value.ToString();
+
+                        string name = sourceDir + item;
+                        if (picList.Contains(name))
+                        {
+                            try
+                            {
+                                File.Copy(Path.Combine(sourceDir, item), Path.Combine(backupDir, item), overwrite);
+                                countSuccess += 1;
+                            }
+                            catch (Exception)
+                            {
+                                continue;
+                            }
+                        }
+                        else
+                        {
+                            countError += 1;
+                            notfound.Add(item);
+                            string data = item + Environment.NewLine;
+                            File.AppendAllText(errorFile, data);
+                        }
+                    }
+                    string[] copiedList = Directory.GetFiles(backupDir);
+                    if (checkBox3.Checked == true)
+                    {
+                        foreach (string f in copiedList)
+                        {
+                            string name = sourceDir + Path.GetFileName(f); ;
+                            File.Delete(name);
+                        }
+                    }
+                    else
+                    {
+
+                    }
+                    watch.Stop();
+                    var elapsedMs = watch.ElapsedMilliseconds;
+
+                    TimeSpan ts = watch.Elapsed;
+                    string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+
+                    MessageBox.Show("Elapsed time" + elapsedTime + Environment.NewLine +
+                        "Copied successfully: " + countSuccess + Environment.NewLine +
+                        "Not found - " + countError + " files: " + Environment.NewLine +
+                        string.Join(Environment.NewLine, notfound.ToArray()), "Error");
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Pleasure select file");
+            }
+        }
+
+        private static void ReadTXTFile()
+        {
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader("TestFile.txt"))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    String line = sr.ReadToEnd();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception eee)
+            {
+                MessageBox.Show("The file could not be read:");
+                MessageBox.Show(eee.Message);
+            }
+        }
+
+        private void CopyWithProgress(string[] filenames)
+        {
+
+            // Display the ProgressBar control.
+            progressBar1.Visible = true;
+            // Set Minimum to 1 to represent the first file being copied.
+            progressBar1.Minimum = 1;
+            // Set Maximum to the total number of files to copy.
+            progressBar1.Maximum = filenames.Length;
+            // Set the initial value of the ProgressBar.
+            progressBar1.Value = 1;
+            // Set the Step property to a value of 1 to represent each file being copied.
+            progressBar1.Step = 1;
+
+            // Loop through all files to copy.
+            //for (int x = 1; x <= filenames.Length; x++)
+            // {
+            // Copy the file and increment the ProgressBar if successful.
+            //if (CopyFile(filenames[x - 1]) == true)
+            //{
+            //    // Perform the increment on the ProgressBar.
+            //    progressBar1.PerformStep();
+            //}
+            //  }
+            progressBar1.PerformStep();
+        }
+
+        private void CopyWithProgress(int count)
+        {
+            string val;
+            progressBar1.Visible = true;
+            // Set Minimum to 1 to represent the first file being copied.
+            progressBar1.Minimum = 1;
+            // Set Maximum to the total number of files to copy.
+
+            //progressBar1.Maximum = filenames.Length;
+            progressBar1.Maximum = count;
+
+            // Set the initial value of the ProgressBar.
+            progressBar1.Value = 1;
+            // Set the Step property to a value of 1 to represent each file being copied.
+            progressBar1.Step = 1;
+            progressBar1.PerformStep();
+
+            //val = progressBar1.Value.ToString();
+            //progressBar1.Increment(+1);
+            //val = progressBar1.Value.ToString();
+        }
+
+        private void open_File_Click(object sender, EventArgs e)
+        {
+            _items.Clear();
+
+            openFileDialog1.Title = "Выберите файл";
+            openFileDialog1.Filter = "Text Files (.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog1.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            DialogResult result = openFileDialog1.ShowDialog();
+            selectfile = Path.GetFileName(openFileDialog1.FileName);
+            label2.Text = selectfile;
+            string[] massiveOfString;
+
+            if (result == DialogResult.OK && selectfile != "")
+            {
+                try
+                {
+                    massiveOfString = System.IO.File.ReadAllLines(selectfile);
+                    foreach (var line in massiveOfString)
+                    {
+                        _items.Add(line);
+                    }
+                }
+                catch (Exception eee)
+                {
+                    MessageBox.Show("The file could not be read:");
+                    MessageBox.Show(eee.Message);
+                }
+
+                listBox1.Items.Clear();
+
+                foreach (var item in _items)
+                {
+                    listBox1.Items.Add(item);
+                }
+            }
+            else
+            {
+                // listBox1.Refresh();
+            }
+
+
+
+        }
+
+
+    }
+}
